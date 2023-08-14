7e2adf96949c22535e0f0eb07b676c7e8d5cc38a
Hoang Anh <calledmeanh@gmail.com>
2023-08-14 14:04:09 +0700

    setup an algorithm to layout appt

--- src/util/appointment.ts
+++ src/util/appointment.ts
@@ -1,4 +1,4 @@
-import { TAppointment } from '../model';
+import { TAppointmentForApp, TAppointmentForUser } from '../model';
 import { TimeUtils } from './time';
 import { CONFIG } from '../constant';
 
@@ -7,62 +7,108 @@ export const AppointmentUtils = {
 };
 
 /* 
-  this algorithm takes a list of appointment and return them in ordered by time, filtered by day
+  this algorithm takes a list of appointment and return the ordered appointments: 
+  https://stackoverflow.com/questions/11311410/visualization-of-calendar-events-algorithm-to-layout-events-with-maximum-width 
  */
 function layoutAlgorithm(
-  appts: TAppointment[],
+  appts: TAppointmentForUser[],
+  opts: {
+    daytimeStart: number;
+    duration: number;
+    columnWidth: number;
+  }
+) {
+  let columns: TAppointmentForApp[][] = [];
+  let lastEventEnding: number | null = null;
+
+  let events: TAppointmentForApp[] = appts.map((appt) => {
+    return { ...appt, ...calcApptPos(appt, { ...opts }), width: 0, left: 0 };
+  });
+
+  // sort it by starting time, and then by ending time.
+  events = events.sort(function (e1, e2) {
+    if (e1.top < e2.top) return -1;
+    if (e1.top > e2.top) return 1;
+    if (e1.top + e1.height < e2.top + e2.height) return -1;
+    if (e1.top + e1.height > e2.top + e2.height) return 1;
+    return 0;
+  });
+
+  // iterate over the sorted array
+  events.forEach((e: TAppointmentForApp) => {
+    if (lastEventEnding !== null && e.top >= lastEventEnding) {
+      packEvents(columns, opts.columnWidth);
+      columns = [];
+      lastEventEnding = null;
+    }
+
+    let placed = false;
+    for (let i = 0; i < columns.length; i++) {
+      let col = columns[i];
+      if (!isOverlapped(col[col.length - 1], e)) {
+        col.push(e);
+        placed = true;
+        break;
+      }
+    }
+
+    if (!placed) {
+      columns.push([e]);
+    }
+
+    if (lastEventEnding === null || e.top + e.height > lastEventEnding) {
+      lastEventEnding = e.top + e.height;
+    }
+  });
+
+  if (columns.length > 0) {
+    packEvents(columns, opts.columnWidth);
+  }
+  return events;
+}
+
+function packEvents(columns: TAppointmentForApp[][], columnWidth: number) {
+  let n = columns.length;
+  for (let i = 0; i < n; i++) {
+    let col = columns[i];
+    for (let j = 0; j < col.length; j++) {
+      let cell = col[j];
+      cell.left = (i / n) * 100;
+      cell.width = columnWidth / n - 1;
+    }
+  }
+}
+
+/* get top, height, startTime, endTime of appt */
+function calcApptPos(
+  appt: TAppointmentForUser,
   opts: {
     daytimeStart: number;
     duration: number;
   }
 ) {
-  // tạm thời bỏ qua lọc ngày
-  // ...
+  const startTime = appt.startTime;
+  const endTime = startTime + TimeUtils.convertMinuteToSeconds(appt.duration);
 
-  /* const apptOrderd = appts.sort((a: TAppointment, b: TAppointment) => a.startTime - b.startTime);
-  console.log('layoutAlgorithm ~ apptOrderd:', apptOrderd); */
+  const top: number = TimeUtils.calcDistanceBetweenTimes(startTime, opts.daytimeStart, opts.duration, 24);
 
-  // tính top, height của appt
-  const apptWithTopHeigt = appts.reduce((init: any, a) => {
-    const top: number = TimeUtils.calcDistanceBetweenTimes(a.startTime, opts.daytimeStart, opts.duration, 24);
-    const endTime = a.startTime + a.duration * CONFIG.SECONDS_PER_MINUTE;
+  const mapDuration = CONFIG.MAPPING_TIME[opts.duration];
+  const height: number = TimeUtils.calcDistanceBetweenTimes(
+    endTime,
+    startTime,
+    mapDuration,
+    (24 * mapDuration) / opts.duration
+  );
 
-    const mapDuration = CONFIG.MAPPING_TIME[opts.duration];
-
-    const height = TimeUtils.calcDistanceBetweenTimes(
-      endTime,
-      a.startTime,
-      mapDuration,
-      (24 * mapDuration) / opts.duration
-    );
-    init.push({
-      ...a,
-      rect: {
-        top,
-        height,
-      },
-    });
-    return init;
-  }, []);
-
-  const apptInOrdered = apptWithTopHeigt.sort((a: TAppointment, b: TAppointment) => a.startTime - b.startTime);
-
-  // check đụng chạm
-  for (let i = 0; i < apptInOrdered.length - 1; i++) {
-    console.log('i')
-    console.log(apptInOrdered[i]);
-    for (let j = i + 1; j < apptInOrdered.length; j++) {
-      console.log('jjj', apptInOrdered[j]);
-    }
-  }
-
-  return apptInOrdered;
-  // làm thé nào để tính width và left?
-  // - left dựa trên ngày (createdAt) của appt đê biết nằm cột nào
-  // - width dựa theo group overlapped của appt
+  return {
+    top,
+    height: height - 1,
+    startTime,
+    endTime,
+  };
 }
 
-function collidesWith(a: any, b: any) {
+function isOverlapped(a: TAppointmentForApp, b: TAppointmentForApp) {
   const { top: start } = a;
   const { top: otherStart } = b;
   const end = start + a.height;
