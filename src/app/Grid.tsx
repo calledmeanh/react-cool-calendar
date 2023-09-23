1b529a6834f0689b4d35ceb337be867d22979027
Hoang Anh <calledmeanh@gmail.com>
2023-09-23 17:20:10 +0700

    move logic of row comp out of grid comp

--- src/app/Grid.tsx
+++ src/app/Grid.tsx
@@ -1,12 +1,9 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { TimeUtils } from '../util';
-import { clsx } from '../util';
-import { Line } from './common';
-import { useCalendarState } from '../hook';
 import NowIndicator from './NowIndicator';
 import Ghost from './Ghost';
 import Appointment from './Appointment';
+import Row from './Row';
 
 const Wrapper = styled.div`
   touch-action: pan-y;
@@ -26,34 +23,12 @@ const Wrapper = styled.div`
 type TGrid = { parentWidth: number };
 
 const Grid: React.FC<TGrid> = ({ parentWidth }) => {
-  const calendarState = useCalendarState();
-
-  const renderRow = useCallback(() => {
-    return TimeUtils.createTimes(calendarState.dayTime.end, calendarState.dayTime.start, calendarState.duration).map(
-      (t, i) => {
-        const currentTime: number = calendarState.dayTime.start + i * calendarState.duration;
-        const workingTime: boolean = TimeUtils.checkWorkingTime(
-          calendarState.dayTime,
-          calendarState.workingTime,
-          currentTime
-        );
-        const groupTime: boolean = TimeUtils.checkGroupTime(calendarState.groupTime, calendarState.duration, i, 'top');
-        const classname = clsx({
-          wt: workingTime,
-          gt: groupTime,
-        });
-        return <Line data-idtf={'line'} className={classname} key={i}></Line>;
-      }
-    );
-  }, [calendarState.duration, calendarState.dayTime, calendarState.workingTime, calendarState.groupTime]);
-
   return (
     <Wrapper data-idtf={'grid'}>
       <NowIndicator type={'LINE'} parentWidth={parentWidth} />
       <Ghost parentWidth={parentWidth} />
       <Appointment parentWidth={parentWidth} />
-
-      {renderRow()}
+      <Row />
     </Wrapper>
   );
 };
---
+++ src/app/Row.tsx
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import { useCalendarState } from '../hook';
+import { TimeUtils, clsx } from '../util';
+import { Line } from './common';
+
+const Wrapper = styled.div`
+  
+`;
+
+const Row: React.FC<{}> = () => {
+  const calendarState = useCalendarState();
+
+  const renderRow = useCallback(() => {
+    return TimeUtils.createTimes(calendarState.dayTime.end, calendarState.dayTime.start, calendarState.duration).map(
+      (t, i) => {
+        const currentTime: number = calendarState.dayTime.start + i * calendarState.duration;
+        const workingTime: boolean = TimeUtils.checkWorkingTime(
+          calendarState.dayTime,
+          calendarState.workingTime,
+          currentTime
+        );
+        const groupTime: boolean = TimeUtils.checkGroupTime(calendarState.groupTime, calendarState.duration, i, 'top');
+        const classname = clsx({
+          wt: workingTime,
+          gt: groupTime,
+        });
+        return <Line data-idtf={'line'} className={classname} key={i}></Line>;
+      }
+    );
+  }, [calendarState.duration, calendarState.dayTime, calendarState.workingTime, calendarState.groupTime]);
+
+  return <Wrapper data-idtf={'row'}>{renderRow()}</Wrapper>;
+};
+
+export default Row;
