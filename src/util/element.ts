4496afc61aa100fd0c023e6540697a60ac7fda0a
Hoang Anh <calledmeanh@gmail.com>
2023-10-13 21:33:01 +0700

    remove calcRectFromMouse func

--- src/util/element.ts
+++ src/util/element.ts
@@ -1,8 +1,5 @@
-import { TRect } from '../model';
-
 export const ElementUtils = {
   getValueFromUserInput,
-  calcRectFromMouse,
   getOffsetToDocument,
 };
 
@@ -11,22 +8,6 @@ export const ElementUtils = {
  */
 function getValueFromUserInput(e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>): string {
   return e && e.target ? e.target.value : '';
-}
-
-function calcRectFromMouse(pageX: number, pageY: number, element: { width: number; height: number }): TRect {
-  const colIdx: number = Math.floor(pageX / element.width);
-  const lineIdx: number = Math.floor(pageY / element.height);
-  const width: number = element.width;
-  const height: number = element.height;
-  const top: number = lineIdx * height;
-  const left: number = colIdx * width;
-
-  return {
-    width,
-    height,
-    top,
-    left,
-  };
 }
 
 function getOffsetToDocument(elem: any, dir: 'left' | 'top'): number {
