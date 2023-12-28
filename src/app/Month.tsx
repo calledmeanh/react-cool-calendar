7031dac9d3143a0e71a55301b100f54551fd1f70
Hoang Anh <calledmeanh@gmail.com>
2023-12-28 23:06:23 +0700

    update month feature: prev-next btn, today-btn, text-today, grid border

--- src/app/Month.tsx
+++ src/app/Month.tsx
@@ -4,24 +4,47 @@ import dayjs, { Dayjs } from "dayjs";
 import { CONFIG } from "../constant";
 import { useCalendarState } from "../hook";
 import { TCalendarStateForApp } from "../model";
+import { DateUtils, clsx } from "../util";
 import { Flex } from "./common";
 import Dateline from "./Dateline";
 
 const DayGrid = styled(Flex)`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
+  border-bottom: 1px solid ${CONFIG.CSS.GRAY_SECONDARY_COLOR};
+  border-left: 1px solid ${CONFIG.CSS.GRAY_SECONDARY_COLOR};
 `;
 
-const DayCell = styled.div<{ $isCurrentMonth: boolean }>`
-  font-size: ${CONFIG.CSS.FONT_SIZE_MEDIUM}px;
-  color: ${(props) => (props.$isCurrentMonth ? "#000" : CONFIG.CSS.FONT_LIGHT_COLOR)};
-  border: 1px solid ${CONFIG.CSS.GRAY_THIRD_COLOR};
-  text-align: center;
-  line-height: 108px;
+const DayCell = styled.div`
   height: 108px;
   cursor: pointer;
+  border-top: 1px solid ${CONFIG.CSS.GRAY_SECONDARY_COLOR};
+  border-right: 1px solid ${CONFIG.CSS.GRAY_SECONDARY_COLOR};
+  background-color: ${CONFIG.CSS.GRAY_PRIMARY_COLOR};
+  &.currMonth {
+    color: #000;
+    background-color: #fff;
+  }
   &:hover {
-    background-color: ${CONFIG.CSS.HIGHLIGHT_SECONDARY_COLOR};
+    background-color: ${CONFIG.CSS.GRAY_PRIMARY_COLOR};
+  }
+`;
+
+const DayText = styled.p`
+  margin: 4px;
+  font-size: ${CONFIG.CSS.FONT_SIZE_MEDIUM}px;
+  color: ${CONFIG.CSS.FONT_LIGHT_COLOR};
+  &.today {
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 50%;
+    background-color: ${CONFIG.CSS.HIGHLIGHT_PRIMARY_COLOR};
+    color: #fff !important;
+  }
+  &.currMonth {
+    color: #000;
   }
 `;
 
@@ -35,18 +58,26 @@ const Month: React.FC = () => {
     const days: JSX.Element[] = [];
 
     for (let i = 1; i <= startIdx; i++) {
-      const prevMonthDay = dayjs(calendarState.currentDate).subtract(1, "month").endOf("month").date() - startIdx + i;
+      const prevMonthDay: number = dayjs(calendarState.currentDate).subtract(1, "month").endOf("month").date() - startIdx + i;
       days.push(
-        <DayCell key={`prev-month-${i}`} $isCurrentMonth={false}>
-          {prevMonthDay}
+        <DayCell key={`prev-month-${i}`}>
+          <DayText>{prevMonthDay}</DayText>
         </DayCell>,
       );
     }
 
     for (let i = 1; i <= daysInMonth; i++) {
+      const isCurrentDay: Dayjs = calendarState.currentDate.date(i);
+      const isToday: boolean = DateUtils.isEqual(isCurrentDay, calendarState.todayGlobalIns);
+
+      const classname: string = clsx({
+        today: isToday,
+        currMonth: true,
+      });
+
       days.push(
-        <DayCell key={i} $isCurrentMonth={true}>
-          {i}
+        <DayCell key={`curr-month-${i}`} className="currMonth">
+          <DayText className={classname}>{i}</DayText>
         </DayCell>,
       );
     }
@@ -54,8 +85,8 @@ const Month: React.FC = () => {
     const remainingDays = 7 - ((startIdx + daysInMonth) % 7);
     for (let i = 1; i <= remainingDays; i++) {
       days.push(
-        <DayCell key={`empty-end-${i}`} $isCurrentMonth={false}>
-          {i}
+        <DayCell key={`next-month-${i}`}>
+          <DayText>{i}</DayText>
         </DayCell>,
       );
     }
@@ -66,7 +97,6 @@ const Month: React.FC = () => {
   return (
     <div data-idtf={CONFIG.DATA_IDTF.MONTH}>
       <Dateline></Dateline>
-
       <DayGrid>{render()}</DayGrid>
     </div>
   );
