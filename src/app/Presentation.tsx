af5bb769aeac9cae0c2a99ae7a72754413188e41
Hoang Anh <calledmeanh@gmail.com>
2023-12-28 23:01:57 +0700

    rewrite the condition

--- src/app/Presentation.tsx
+++ src/app/Presentation.tsx
@@ -9,7 +9,7 @@ const Presentation: React.FC = () => {
 
   return (
     <React.Fragment>
-      {(calendarState.viewMode !== "MONTH") && <Scrolling />}
+      {calendarState.viewMode !== "MONTH" && <Scrolling />}
       {calendarState.viewMode === "MONTH" && <Month />}
     </React.Fragment>
   );
