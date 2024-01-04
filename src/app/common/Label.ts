e5d826b10d7ac817a3d5c931b15a2ec1a0592c22
Hoang Anh <calledmeanh@gmail.com>
2024-01-04 22:17:09 +0700

    rename Text to Label comp

--- src/app/common/Label.ts
+++ src/app/common/Label.ts
@@ -3,7 +3,7 @@ import { CONFIG } from "../../constant";
 import { TStylePadding, TStyleBorderRadius } from "../../model";
 import { Flex } from "./Flex";
 
-export const Text = styled(Flex)<{ $padding: TStylePadding; $isborderradius?: TStyleBorderRadius }>`
+export const Label = styled(Flex)<{ $padding: TStylePadding; $isborderradius?: TStyleBorderRadius }>`
   padding: ${(props) => `${props.$padding[0]}px ${props.$padding[1]}px`};
   border-radius: ${(props) => (props.$isborderradius ? `4px` : 0)};
   border: 1px solid ${CONFIG.CSS.GRAY_THIRD_COLOR};
