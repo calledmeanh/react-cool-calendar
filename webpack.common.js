d93f7f781fd714c4964656fc19ffa5e2af36c91b
Hoang Anh <calledmeanh@gmail.com>
2024-01-25 13:42:15 +0700

    remove hot module because in built-in

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "docs", "index.html"),
     }),
-    new MiniCssExtractPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin()
   ],
 };
